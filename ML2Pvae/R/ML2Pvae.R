4f158bcb8f625176106659dea2dbebcf1236347e
Geoff Converse <converseg@gmail.com>
2019-10-24 13:22:22 -0500

    created vignette

--- ML2Pvae/R/ML2Pvae.R
+++ ML2Pvae/R/ML2Pvae.R
@@ -3,7 +3,10 @@
 #' 
 #' The ML2Pvae package includes functions which build a VAE with the desired architecture, and
 #' fits the latent skills to either a standard normal (independent) distrubution,
-#' or a multivariate normal distribution with a full covariance matrix.
+#' or a multivariate normal distribution with a full covariance matrix. Based on the work 
+#' "Interpretable Variational Autoencdoers for Cognitive Models"
+#' by Curi, M., Converse, G., Hajewski, J., and Oliveira, S. 
+#' Found in International Joint Conference on Neural Networks, 2019.
 #'
 #' @docType package
 #' @name ML2Pvae
