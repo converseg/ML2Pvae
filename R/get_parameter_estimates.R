befc0b3e5b0b3c2481304451d490ab4537b42d90
Geoff Converse <converseg@gmail.com>
2020-09-03 00:16:18 -0500

    added functions to more easily reover parameters

---
+++ R/get_parameter_estimates.R
@@ -0,0 +1,54 @@
+#' Get trainable variables from the decoder, which serve as item parameter estimates.
+#' 
+#' @param decoder a trained keras model
+#' @param model_type either 1 or 2, specifying a 1 parameter (1PL) or 2 parameter (2PL) model
+#' @return a list which contains item parameter estimates. The length of this list is equal to model_type
+#' @export
+#' @examples
+#' Q <- matrix(c(1,0,1,1,0,1,1,0), nrow = 2, ncol = 4)
+#' models <- build_vae_standard_normal(4, 2, Q, model_type = 2)
+#' decoder <- models[[2]]
+#' item_parameter_estimates <- get_item_parameter_estimates(decoder, model_type = 2)
+get_item_parameter_estimates <- function(decoder, model_type = 2){
+  all_decoder_weights <- keras::get_weights(decoder)
+  weights_length <- length(all_decoder_weights)
+  estimates <- c()
+  if (model_type == 1){
+    estimates[1] <- all_decoder_weights[weights_length]
+  } else{
+    estimates[1] <- all_decoder_weights[weights_length - 1]
+    estimates[2] <- all_decoder_weights[weights_length]
+  }
+  estimates
+}
+
+#' Feed forward response sets through the encoder, which outputs student ability estimates
+#' 
+#' @param encoder a trained keras model
+#' @param responses a set of binary responses, as used in training
+#' @return a list containing student ability estimates and the variance (or covariance matrix) of those estimates
+#' @export
+#' @examples
+#' data <- matrix(c(1,1,0,0,1,0,1,1,0,1,1,0), nrow = 3, ncol = 4)
+#' Q <- matrix(c(1,0,1,1,0,1,1,0), nrow = 2, ncol = 4)
+#' models <- build_vae_standard_normal(4, 2, Q, model_type = 2)
+#' encoder <- models[[1]]
+#' ability_parameter_estimates <- get_ability_parameter_estimates(encoder, data)
+get_ability_parameter_estimates <- function(encoder, responses){
+  encoded_responses <- encoder(responses)
+  estimates_variances <- c()
+  ability_parameter_estimates <- encoded_responses[[1]]
+  ability_parameter_log_variance <- encoded_responses[[2]]
+  if (ability_parameter_estimates$shape == ability_parameter_log_variance$shape){
+    estimates_variances[[1]] <- ability_parameter_estimates$numpy()
+    estimates_variances[[2]] <- exp(ability_parameter_log_variance$numpy())
+  } else{
+    b <- tfprobability::tfb_fill_triangular(upper=FALSE)
+    log_cholesky <- b$forward(ability_parameter_log_variance)
+    cholesky <- tensorflow::tf$linalg$expm(log_cholesky)
+    cov_matrices <- tensorflow::tf$matmul(cholesky, tensorflow::tf$transpose(cholesky, c(0L, 2L, 1L)))
+    estimates_variances[[1]] <- ability_parameter_estimates$numpy()
+    estimates_variances[[2]] <- cov_matrices
+  }
+  estimates_variances
+}
--- tests/testthat/test_training.R
+++ tests/testthat/test_training.R
@@ -13,11 +13,11 @@ test_that("standard normal vae can be fit to data", {
   decoder <- models[[2]]
   vae <- models[[3]]
   history <- train_model(vae, train_data, validation_split = 0.1, num_epochs = 3, verbose=0)
-  encoded_test <- predict(encoder, test_data)
-  enc_test_mean <- encoded_test[[1]]
-  enc_test_log_var <- encoded_test[[2]]
+  estimates <- get_ability_parameter_estimates(encoder, test_data)
+  enc_test_mean <- estimates[[1]]
+  enc_test_var <- estimates[[2]]
   expect_equal(dim(enc_test_mean),c(3,2))
-  expect_equal(dim(enc_test_log_var),c(3,2))
+  expect_equal(dim(enc_test_var),c(3,2))
 })
 
 test_that("full covariance vae can be fit to data", {
@@ -37,11 +37,11 @@ test_that("full covariance vae can be fit to data", {
   decoder <- models[[2]]
   vae <- models[[3]]
   history <- train_model(vae, train_data, validation_split = 0.1, batch_size = 3, num_epochs = 3, verbose=0)
-  encoded_test <- encoder(test_data)
-  enc_test_mean <- encoded_test[[1]]
-  enc_test_log_chol <- encoded_test[[2]]
+  estimates <- get_ability_parameter_estimates(encoder, test_data)
+  enc_test_mean <- estimates[[1]]
+  enc_test_cov <- estimates[[2]]
   expect_equal(dim(enc_test_mean),c(3,2))
-  expect_equal(dim(enc_test_log_chol), c(3,3))
+  expect_equal(dim(enc_test_cov), c(3,2,2))
 })
 
 test_that("1-parameter logistic model option makes all decoder weights = 1", {
@@ -66,10 +66,11 @@ test_that("single-dimensional IRT estimation works", {
   responses[responses <= 0.5] <- 0
   data <- matrix(responses, nrow = 10, ncol = 3)
   Q <- matrix(rep(1,3), nrow = 1, ncol = 3)
-  models <- build_vae_standard_normal(3, 1, Q, model_type = 1, enc_hid_arch = c(4,2))
+  models <- build_vae_standard_normal(3, 1, Q, model_type = 2, enc_hid_arch = c(4,2))
   encoder <- models[[1]]
   decoder <- models[[2]]
   vae <- models[[3]]
   history <- train_model(vae, data, num_epochs = 3, verbose=0)
+  item_parameter_estimates <- get_item_parameter_estimates(decoder, model_type = 2)
   expect_equal(1,1)
 })
