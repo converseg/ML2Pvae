023350d91cc22544965f69b9e3af376668236320
Geoff Converse <converseg@gmail.com>
2020-10-28 12:21:34 -0500

    added cran-comments to Rbuildignore and commented out packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,9 @@
 #' @param libnam the library name
 #' @param pkgname the package name
 .onLoad <- function(libnam, pkgname){
-  packageStartupMessage("Thank you for installing ML2Pvae.\nBe sure that the following Python libraries have been installed:
-    tensorflow
-    keras
-    tensorflow-probability\nWe also recommend using the developer, rather than CRAN, version of keras for R.\nThis can be installed with devtools::install_github('rstudio/keras')")
+  # packageStartupMessage gives a NOTE on winbuilder, causing CRAN submission to fail auto checks
+  # packageStartupMessage("Thank you for installing ML2Pvae.\nBe sure that the following Python libraries have been installed:
+  #   tensorflow
+  #   keras
+  #   tensorflow-probability\nWe also recommend using the developer, rather than CRAN, version of keras for R.\nThis can be installed with devtools::install_github('rstudio/keras')")
 }
